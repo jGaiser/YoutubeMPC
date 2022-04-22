b634a982627ddab8bc17d737222c16a86aae69e4
Jeremiah Gaiser <jgaiser406@gmail.com>
2022-04-21 20:07:59 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import reactDom from 'react-dom';
+import ReactDOM from 'react-dom';
+
+function YoutubeMPCApp(props) {
+  return (
+    <div id='pageApp'>
+      <div className='containerTop'>
+        
+      </div>
+      <div className='containerBottom'>
+
+      </div>
+    </div>
+  );
+}
+
+<div id='root'></div>
+
+ReactDOM.render(
+  <YoutubeMPCApp />,
+  document.getElementById('root')
+)
